db9af9406bbf53b38681e4a3eb2d452164fd1bde
OscarW1 <162868@chrome.escg.ac.uk>
2020-03-02 15:18:40 +0000

    Added a button at the start of the game to make the walls dissapear

---
+++ FMP Task/Assets/Scripts/BeginGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginGame : MonoBehaviour
+
+{
+    public GameObject[] TilestoRemove;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        foreach(GameObject i in TilestoRemove)
+        {
+            Destroy(i);
+        }
+
+        Destroy(this);
+    }
+
+}
