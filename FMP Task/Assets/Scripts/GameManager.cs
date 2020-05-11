b1ea184f9fedff8b521fbeedf55d582355db95ad
OscarW1 <162868@chrome.escg.ac.uk>
2020-05-11 11:19:32 +0100

    Added a new script and a new scene

--- FMP Task/Assets/Scripts/GameManager.cs
+++ FMP Task/Assets/Scripts/GameManager.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject completeLevelUI;
+    public string Scenetogoto;
 
 
     public void CompleteLevel()
     {
-        completeLevelUI.SetActive(true);
+        SceneManager.LoadScene(Scenetogoto);
     }
 }
